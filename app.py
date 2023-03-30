08ca36fe9150d5a71fc4ef7141003213a359f1ed
hectordob <hectordob@gmail.com>
2023-03-30 16:16:32 +0200

    actualización novedades

--- app.py
+++ app.py
@@ -28,6 +28,21 @@ def login_required(f):
 
     return check_token
 
+def admin_required(f):
+    @wraps(f)
+    def check_rol(id=None, *args, **kwargs):
+        if session['rol'] != 'admin':
+
+            flash('Se requieren permisos especiales')
+            return redirect(url_for('home'))
+        else:
+            if id is None:
+                return f(*args, **kwargs)
+            else:
+                return f(id, *args, **kwargs)
+    return check_rol
+
+
 
 @app.route('/home')
 def get_admin():
@@ -93,10 +108,142 @@ def login():
             return redirect(url_for('login'))
 
 
+@app.route('/articulos/')
+@login_required
+def mostrar_articulos():
+    datos = funcionesbbdd.show_articulos(rol='admin')
+    mis_articulos = funcionesbbdd.funcionesbbdd.own_articulos(idprestador=session['idusuario'])
+    idsarticulos = [articulo['idarticulo'] for articulo in mis_articulos]
+    return render_template('mostrar_articulos.html',
+                           datos=datos,
+                           idsarticulos=idsarticulos,
+                           idusuario=session['idusuario'],
+                           rol=session['rol'])
 
-@app.route('/articulos')
-def articulos():
-    return render_template('articulos.html')
+
+@app.route('/articulos/<idarticulo>/')
+@login_required
+def mostrar_articulo(idarticulo):
+    datos = funcionesbbdd.show_articulos(idarticulo=idarticulo, rol='admin')
+    mis_articulos = funcionesbbdd.funcionesbbdd.own_articulos(idprestador=session['idusuario'])
+    idsarticulos = [articulo['idarticulo'] for articulo in mis_articulos]
+    return render_template('mostrar_articulos.html',
+                           datos=datos,
+                           idsarticulos=idsarticulos,
+                           idusuario=session['idusuario'],
+                           rol=session['rol'])
+
+
+@app.route('/articulos/mis_articulos/')
+@login_required
+def mis_articulos():
+    if session['rol'] == 'user':
+        datos = funcionesbbdd.funcionesbbdd.own_articulos(session['idusuario'])
+        return render_template('mostrar_articulos.html', datos)
+    return redirect(url_for('mostrar_articulos'))
+
+
+@app.route('/articulos/crear/', methods=['GET', 'POST'])
+@login_required
+def crear_articulo():
+    if request.method == 'GET':
+        return render_template('add_article.html')
+    else:
+        idprestador = session['idusuario']
+        titulo = request.form['titulo']
+        autor = request.form['autor']
+        editorial = request.form['editorial']
+        year = request.form['year']
+        categoria = request.form['categoria']
+        edad_min = request.form['edad_min']
+        descripcion = request.form['descripcion']
+
+        imagen1 = request.files['imagen1']
+        filename = secure_filename(imagen1.filename)
+        imagen1.save(f'./static/images/{filename}')  # DESDE PERSPECTIVA DE APP
+        imagen1 = f'/static/images/{filename}'  # DESDEPERSPECTIVA DE HTML
+        # imagen1.save(path.join(app.config['UPLOAD_FOLDER'], filename))
+        # imagen1 = path.join(app.config['UPLOAD_FOLDER'], filename)
+
+        funcionesbbdd.create_articulo(idprestador, titulo, autor, editorial, year, categoria, edad_min, descripcion, imagen1)
+
+        idarticulo = funcionesbbdd.show_articulos(rol='admin')[-1]['idarticulo']
+        return redirect(url_for('mostrar_articulo', idarticulo=idarticulo))
+        # return redirect(url_for('articulos'))
+
+
+@app.route('/articulos/<idarticulo>/borrar/')
+@login_required
+def borrar_articulo(idarticulo):
+    print('permiso aceptado')
+    funcionesbbdd.delete_articulo(idarticulo=idarticulo)
+    return redirect(url_for('articulos'))
+
+
+@app.route('/articulos/<idarticulo>/actualizar/', methods=['GET', 'POST'])
+@login_required
+def actualizar_articulo(idarticulo):
+    if request.method == 'GET':
+        datos = funcionesbbdd.show_articulos(rol=session['rol'])
+        return render_template('actualizar_articulo.html', datos=datos)
+    else:
+        idprestador = session['idusuario']
+        titulo = request.form['titulo']
+        autor = request.form['autor']
+        editorial = request.form['editorial']
+        year = request.form['year']
+        categoria = request.form['categoria']
+        edad_min = request.form['edad_minima']
+        descripcion = request.form['descripcion']
+        imagen1 = request.files['imagen1']
+        print('antes', imagen1)
+        imagen1.save(secure_filename(imagen1.filename))
+        imagen1 = imagen1.filename
+        print('despues', imagen1)
+        funcionesbbdd.create_articulo(idprestador, titulo, autor, editorial, year, categoria, edad_min, descripcion, imagen1)
+        return redirect(url_for('mostrar_articulo', idarticulo=idarticulo))
+        # return redirect(url_for('articulos'))
+
+
+# ====================================================================================================
+# 9 - PRESTAMOS
+# ====================================================================================================
+@app.route('/prestamos/')
+@login_required
+@admin_required
+def mostrar_prestamos():
+    funcionesbbdd.show_prestamos(rol=session['rol'])
+    return render_template('mostrar_prestamos.html')
+
+
+@app.route('/prestamos/<idprestamo>/')
+@login_required
+def mostrar_prestamo(idprestamo):
+    datos = funcionesbbdd.show_prestamos(idprestamo=idprestamo, rol=session['rol'])
+    return render_template('mostrar_prestamos.html', datos=datos)
+
+
+@app.route('/prestamos/mis_prestamos/')
+@login_required
+def mis_prestamos():
+    if session['rol'] == 'user':
+        datos = own_prestamos(session['idusuario'])
+        return render_template('mostrar_prestamos.html', datos=datos)
+    return redirect(url_for('mostrar_prestamos'))
+
+
+""" CREAR: UN PRESTAMO NO SE CREA CON UN FORMULARIO, SE EJECUTA SI SE DA A UN BOTON """
+
+
+@app.route('/prestamos/crear/')
+@login_required
+def crear_prestamo():
+    idusuario = session['idusuario']
+    idarticulo = request.args['idarticulo']
+    funcionesbbdd.create_prestamo(idusuario, idarticulo)
+
+    idprestamo = funcionesbbdd.show_prestamos(rol=session['rol'])[-1]['idprestamo']
+    return redirect(url_for('prestamo', idprestamo=idprestamo))
 
 
 
--- funcionesbbdd.py
+++ funcionesbbdd.py
@@ -34,3 +34,145 @@ def check(usuario, password):
         else:
             final = False
     return final
+
+
+def database_interaction(sql=None, values=None, get_result=False):
+    conn = sqlite3.connect('maresme.sqlite')
+    if sql is not None:
+        if values is None:
+            cursor = conn.execute(sql)
+        else:
+            cursor = conn.execute(sql, values)
+        if get_result is True:
+            colnames = [row[0] for row in cursor.description]
+            elements = []
+            for i, element in enumerate(cursor):
+                elements.append({})
+                for j, colname in enumerate(colnames):
+                    elements[i][colname] = element[j]
+            conn.commit()
+            conn.close()
+            return elements
+        conn.commit()
+        conn.close()
+
+
+def show_user(idusuario=None, rol=None):
+    if idusuario:
+        if rol == 'admin':
+            sql = 'SELECT * FROM usuarios WHERE idusuario=?'
+            return database_interaction(sql=sql, values=[idusuario], get_result=True)
+        sql = 'SELECT nombre, apellido1, apellido2, fecha_nac, email, telefono, fecha_alta, usuario, password FROM usuarios WHERE idusuario=?'
+        return database_interaction(sql=sql, values=[idusuario], get_result=True)
+    else:
+        if rol == 'admin':
+            sql = 'SELECT * FROM usuarios'
+            return database_interaction(sql=sql, get_result=True)
+        sql = 'SELECT nombre, apellido1, apellido2, fecha_nac, email, telefono, fecha_alta, usuario, password FROM usuarios'
+        return database_interaction(sql=sql, get_result=True)
+
+
+def update_user(nombre, apellido1, apellido2, fecha_nac, email, telefono, usuario, password, id):
+    values = [nombre, apellido1, apellido2, fecha_nac, email, telefono, usuario, generate_password_hash(password), id]
+    sql = 'UPDATE usuarios SET nombre=?, apellido1=?, apellido2=?, fecha_nac=?, email=?, telefono=?, usuario=?, password=? WHERE idusuario=?'
+    database_interaction(sql=sql, values=values)
+
+
+def delete_user(idusuairo):
+    values = [idusuairo]
+    sql = 'DELETE FROM usuarios WHERE idusuario=?'
+    database_interaction(sql=sql, values=values)
+
+
+def create_articulo(idprestador, titulo, autor, editorial, year, categoria, edad_min, descripcion, imagen1):
+    values = [idprestador, titulo, autor, editorial, year, categoria, edad_min, descripcion, imagen1]
+    sql = 'INSERT INTO articulos(idprestador, titulo, autor, editorial, year, categoria, edad_min, descripcion, imagen1) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)'
+    database_interaction(sql, values)
+
+
+def show_articulos(idarticulo=None, rol=None):
+    if idarticulo:
+        if rol == 'admin':
+            sql = 'SELECT * FROM articulos WHERE idarticulo=?'
+            return database_interaction(sql=sql, values=[idarticulo], get_result=True)
+        sql = 'SELECT titulo, autor, editorial, year, categoria, edad_min, descripcion, imagen1 FROM articulos WHERE idarticulo=?'
+        return database_interaction(sql=sql, values=[idarticulo], get_result=True)
+    else:
+        if rol == 'admin':
+            sql = 'SELECT * FROM articulos'
+            return database_interaction(sql=sql, get_result=True)
+        sql = 'SELECT titulo, autor, editorial, year, categoria, edad_min, descripcion, imagen1 FROM articulos'
+        return database_interaction(sql=sql, get_result=True)
+
+
+def update_articulo(titulo, autor, editorial, year, categoria, edad_min, descripcion, imagen1, idarticulo):
+    values = [titulo, autor, editorial, year, categoria, edad_min, descripcion, imagen1, idarticulo]
+    sql = 'UPDATE articulos SET titulo=?, autor=?, editorial=?, year=?, categoria=?, edad_min=?, descripcion=?, imagen1=? WHERE idarticulo=?'
+    database_interaction(sql=sql, values=values)
+
+
+def delete_articulo(idarticulo):
+    sql = 'DELETE FROM articulos WHERE idarticulo=?'
+    values = [idarticulo]
+    database_interaction(sql, values)
+
+
+def own_articulos(idprestador):
+    sql = 'SELECT * FROM articulos WHERE idprestador=?'
+    return database_interaction(sql=sql, values=[idprestador], get_result=True)
+
+
+def show_articulos_user(idprestador=None, rol=None):
+    if ideprestador:
+        if rol == 'admin':
+            sql = 'SELECT * FROM articulos WHERE idprestador=?'
+            return database_interaction(sql=sql, values=[idprestador], get_result=True)
+        sql = 'SELECT titulo, autor, editorial, year, categoria, edad_min, descripcion, imagen1 FROM articulos WHERE idprestador=?'
+        return database_interaction(sql=sql, values=[idarticulo], get_result=True)
+
+
+# ====================================================================================================
+# 2 - PRESTAMOS
+# ====================================================================================================
+def create_prestamo(idusuario, idarticulo):
+    fecha_ini = date.today()
+    values = [idusuario, idarticulo, fecha_ini]
+    sql = 'INSERT INTO prestamos(idusuario, idarticulo, fecha_ini) VALUES (?, ?, ?)'
+    database_interaction(sql, values)
+
+
+def show_prestamos(idprestamo=None, rol=None):
+    if idprestamo:
+        if rol == 'admin':
+            sql = 'SELECT * FROM prestamos WHERE idprestamo=?'
+            return database_interaction(sql=sql, values=[idprestamo], get_result=True)
+        sql = 'SELECT fecha_ini, fecha_fin, devuelto, observaciones FROM prestamos WHERE idprestamo=?'
+        return database_interaction(sql=sql, values=[idprestamo], get_result=True)
+    else:
+        if rol == 'admin':
+            sql = 'SELECT * FROM prestamos'
+            return database_interaction(sql=sql, get_result=True)
+        sql = 'SELECT fecha_ini, fecha_fin, devuelto, observaciones FROM prestamos WHERE idprestamo=?'
+        return database_interaction(sql=sql, get_result=True)
+
+
+def update_prestamo(idusuario, idarticulo, fecha_ini, devuelto, observaciones, idprestamo):
+    if devuelto == 1:
+        fecha_fin = date.today()
+        sql = 'UPDATE prestamos SET idusuario=?, idarticulo=?, fecha_ini=?, fecha_fin=?, devuelto=?, observaciones=? WHERE idprestamo=?'
+        database_interaction(sql,
+                             values=[idusuario, idarticulo, fecha_ini, fecha_fin, devuelto, observaciones, idprestamo])
+    else:
+        sql = 'UPDATE prestamos SET idusuario=?, idarticulo=?, fecha_ini=?, devuelto=?, observaciones=? WHERE idprestamo=?'
+        database_interaction(sql, values=[idusuario, idarticulo, fecha_ini, devuelto, observaciones, idprestamo])
+
+
+def delete_prestamo(idprestamo):
+    sql = 'DELETE FROM prestamos WHERE idprestamo=?'
+    values = [idprestamo]
+    database_interaction(sql, values)
+
+
+def own_prestamos(idprestante):
+    sql = 'SELECT * FROM prestamos WHERE idprestante=?'
+    return database_interaction(sql=sql, values=[idprestante], get_result=True)
